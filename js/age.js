68f495823668dd54622294dd6744e234408be6f7
4kord <57727313+4kord@users.noreply.github.com>
2022-08-24 01:38:57 +0300

    init

---
+++ js/age.js
@@ -0,0 +1,6 @@
+const ageEl = document.querySelector("#age");
+
+setInterval(() => {
+    let time = dayjs().diff(dayjs("2004-06-02"), 'year', true);
+    ageEl.innerText = time.toString().substring(0, 12);
+}, 50);
---
+++ js/intro.js
@@ -0,0 +1,35 @@
+let header = document.querySelector("#intro");
+let anim = [
+    { t: " ", ms: 200 },
+    { t: "_", ms: 200 },
+    { t: " ", ms: 200 },
+    { t: "_", ms: 200 },
+    { t: "4_", ms: 100 },
+    { t: "4K_", ms: 100 },
+    { t: "4KO_", ms: 100 },
+    { t: "4KOR_", ms: 100 },
+    { t: "4KORD_", ms: 100 },
+    { t: "4KORD ", ms: 200 },
+    { t: "4KORD_", ms: 200 },
+    { t: "{4KORD}", ms: 400 },
+];
+
+let i = 0;
+
+let update = () => {
+    let step = anim[i];
+    header.innerText = step.t;
+    i++;
+
+    if (i < anim.length)
+        setTimeout(update, step.ms);
+    else {
+        header.classList.add("top");
+        setTimeout(() => {
+            document.querySelector(".main").style.opacity = 1;
+        }, 500);
+        i = 0;
+        setTimeout(updateTag, 5000);
+    }
+}
+update();
